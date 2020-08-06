a4bfd4b7a51552f3d94ec847e9e0608f86eb189d
Ann-Patrice Hickey <aphickey@tiac.net>
2020-08-06 12:22:28 -0400

    Create main.py
    first python file committed. We'll see

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def print_hi(name):
+
+    name = "Ron"
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print("   /|")
+    print("  / |")
+    print(" /  |")
+    print("/___|")
+
+if __name__ == '__main__':
+    print_hi('Hello World!')
+
